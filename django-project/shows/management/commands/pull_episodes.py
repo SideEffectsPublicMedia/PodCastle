4a7fc7cab8a780d43d9a615675727d5f837c677c
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-07-24 13:16:07 -0500

    Migrated show and made command.

---
+++ django-project/shows/management/commands/pull_episodes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Pull podcast episodes over Story API.
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Pull podcast episodes over Story API.
+    """
+
+    help = 'Pull podcast episodes over Story API.'
+
+    def handle(self, *args, **options):
+        """
+        Make it happen.
+        """
+        return
