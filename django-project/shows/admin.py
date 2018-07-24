77ef1718f8cbb754536a52d624fbcdcb6a23071c
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-07-24 12:13:11 -0500

    Model admins.

--- django-project/shows/admin.py
+++ django-project/shows/admin.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from shows import models
 
 # Register your models here.
+
+class EpisodeInline(admin.StackedInline):
+    model = models.Episode
+
+@admin.register(models.Show)
+class ShowAdmin(admin.ModelAdmin):
+    inlines = [
+        EpisodeInline,
+    ]
+
+@admin.register(models.Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    pass
