b3d5e406654a6d49a8988992949cbb06b015f836
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-08-21 23:52:40 -0500

    Accidentally included logger line.

--- django-project/pull_update.sh
+++ django-project/pull_update.sh
@@ -4,5 +4,3 @@ source /root/.bashrc
 /root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py pull_episodes
 /root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py build
 /root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py publish
-
-*/15 * * * * sh /root/sfx-pc/django-project/pull_update.sh 2>&1 | logger --tag "pc-sfx"
