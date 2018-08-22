fb8c4b085c19141e08cc3cd8bad0e4f4900db8ab
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-08-21 23:42:43 -0500

    Removed logging defaults.

--- django-project/pull_update.sh
+++ django-project/pull_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Server-side pull, publish and log.
-/root/.virtualenvs/pc-sfx/bin/python manage.py pull_episodes 2>&1 | logger --tag "pc-sfx"
-/root/.virtualenvs/pc-sfx/bin/python manage.py build 2>&1 | logger --tag "pc-sfx"
-/root/.virtualenvs/pc-sfx/bin/python manage.py publish 2>&1 | logger --tag "pc-sfx"
+/root/.virtualenvs/pc-sfx/bin/python manage.py pull_episodes
+/root/.virtualenvs/pc-sfx/bin/python manage.py build
+/root/.virtualenvs/pc-sfx/bin/python manage.py publish
