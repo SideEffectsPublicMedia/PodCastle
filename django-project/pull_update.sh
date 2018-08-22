89bc926f3e11edcb2f4614ea00b7c467179f2603
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-08-21 23:11:04 -0500

    Complete command.

--- django-project/pull_update.sh
+++ django-project/pull_update.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # Server-side pull, publish and log.
-/root/.virtualenvs/pc-sfx/bin/python manage.py pull_episodes 2>&1 | logger &
+/root/.virtualenvs/pc-sfx/bin/python manage.py pull_episodes 2>&1 | logger --tag "pc-sfx"
+/root/.virtualenvs/pc-sfx/bin/python manage.py build 2>&1 | logger --tag "pc-sfx"
+/root/.virtualenvs/pc-sfx/bin/python manage.py publish 2>&1 | logger --tag "pc-sfx"
