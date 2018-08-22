bef9549a9bce9dc984036d0fd2f4341e5f886c16
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-08-21 23:44:25 -0500

    Another foolish mistake.

--- django-project/pull_update.sh
+++ django-project/pull_update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Server-side pull, publish and log.
-/root/.virtualenvs/pc-sfx/bin/python manage.py pull_episodes
-/root/.virtualenvs/pc-sfx/bin/python manage.py build
-/root/.virtualenvs/pc-sfx/bin/python manage.py publish
+/root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py pull_episodes
+/root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py build
+/root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py publish
+
+*/15 * * * * sh /root/sfx-pc/django-project/pull_update.sh 2>&1 | logger --tag "pc-sfx"
