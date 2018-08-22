06153627e341b11eb5512815a1304554970ded8b
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2018-08-21 23:51:34 -0500

    Added source because I'm getting desperate.

--- django-project/pull_update.sh
+++ django-project/pull_update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Server-side pull, publish and log.
+source /root/.bashrc
 /root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py pull_episodes
 /root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py build
 /root/.virtualenvs/pc-sfx/bin/python /root/sfx-pc/django-project/manage.py publish
